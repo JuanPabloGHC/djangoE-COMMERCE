ba4e9fe1925c3e4209c61636c1f7f3f7261bb861
JuanPabloGHC <jpghc552@hotmail.com>
2023-04-26 23:35:05 -0600

    update, delete

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,6 @@ from .views import *
 urlpatterns = [
     path('', Products.as_view(), name='product'),
     path('create', CreateProduct.as_view(), name='create_product'),
+    path('update/<int:pk>', UpdateProduct.as_view(), name='update_product'),
+    path('delete/<int:pk>', DeleteProduct.as_view(), name='delete_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views import View
 from products.forms import ProductCreate
 from products.models import Product
@@ -25,3 +25,35 @@ class CreateProduct(View):
             print("FORM ERROR!")
             print(form.errors)
             return render(request, 'products/products.html', {'form': form})
+
+class UpdateProduct(View):  
+    def get(self, request, pk):
+        product = Product.objects.get(id=pk)
+        form = ProductCreate(instance=product)
+
+        context = {'form': form}
+        return render(request, 'products/form.html', context)
+          
+    def post(self, request, pk):
+
+        product = get_object_or_404(Product, id=pk)
+
+        form = ProductCreate(request.POST, instance = product)
+
+        if form.is_valid():
+            form.save()
+            return redirect('product')
+        else:
+            print("FORM ERROR!")
+            print(form.errors)
+            return render(request, 'products/products.html', {'form': form})
+
+class DeleteProduct(View):
+    def get(self, request, pk):
+        product = Product.objects.get(id=pk)
+        print(product)
+        product.delete()
+        return redirect('product')
+        #products = Product.objects.all()
+        #context = {'products': products}
+        #return render(request, 'products/products.html', context)
